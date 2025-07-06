40b80d529bed5656d4b780409d34d30c8ba798e9
kamrul islam <cdkamrul9@gmail.com>
2025-07-06 21:33:15 +0600

    first commit

---
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -0,0 +1,43 @@
+import scrapy
+
+
+class BookspiderSpider(scrapy.Spider):
+    name = "bookspider"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com"]
+
+    def parse(self, response):
+        books = response.css("article.product_pod")
+        for book in books:
+            # yield {
+                # "name": book.css("h3 a::attr(title)").get(),
+                # "price": book.css("p.price_color::text").get(),
+                # "url": book.css("h3 a::attr(href)").get(),
+                # "image_url": book.css("img.thumbnail::attr(src)").get(),
+                # "availability": book.css("p.availability::text").re_first(r"\d+"),
+            relative_url = book.css("h3 a::attr(href)").get()
+            if 'catalogue/' in relative_url:
+                book_url = 'https://books.toscrape.com/' + relative_url
+            else:
+                book_url = 'https://books.toscrape.com/catalogue/' + relative_url
+            yield response.follow(book_url, callback = self.parse_book_page)
+            # }
+
+        # next_page = response.css("li.next a::attr(href)").get()
+        # if next_page is not None:
+        #     if 'catalogue/' in next_page:
+        #         next_page_url = 'https://books.toscrape.com/' + next_page
+        #     else:
+        #         next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+        #     yield response.follow(next_page_url, callback = self.parse)
+# This spider scrapes book information from the website "books.toscrape.com".
+# It extracts the book name, price, URL, image URL, and availability.
+    def parse_book_page(self, response):
+        book = {
+            "name": response.css("h1::text").get(),
+            "price": response.css("p.price_color::text").get(),
+            "url": response.url,
+            "image_url": response.css("img.thumbnail::attr(src)").get(),
+            "availability": response.css("p.availability::text").re_first(r"\d+"),
+        }
+        yield book
